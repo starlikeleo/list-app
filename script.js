f27784baefbfbdb88a575005dde603b49faed143
ryo nakagawa <starlikeleon@gmail.com>
2023-03-11 06:22:22 +0900

    number_change

--- script.js
+++ script.js
@@ -42,3 +42,16 @@ console.log(length);
 
 // 変数lengthを用いて、円の面積を出力してください
 console.log(length * length * 3);
+
+let number = 7;
+console.log(number);
+
+// 変数numberの値に3を加えてください
+number += 3;
+
+console.log(number);
+
+// 変数numberの値を2で割ってください
+number /= 2;
+
+console.log(number);
