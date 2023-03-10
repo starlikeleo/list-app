f5733ba684294b80b0f135758ca3957bffd6f091
ryo nakagawa <starlikeleon@gmail.com>
2023-03-11 06:25:35 +0900

    add_const-language

--- script.js
+++ script.js
@@ -55,3 +55,12 @@ console.log(number);
 number /= 2;
 
 console.log(number);
+
+// 定数languageを定義してください
+const language ="フランス語";
+
+// 定数languageの値を出力してください
+console.log(language);
+
+// 定数languageを用いて、「〇〇を話せます」と出力してください
+console.log(language + "を話せます");
