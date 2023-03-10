7682f7ff78a55eadd8675cb132a2d32407b2a70e
ryo nakagawa <starlikeleon@gmail.com>
2023-03-11 06:15:29 +0900

    add_length

--- script.js
+++ script.js
@@ -27,3 +27,12 @@ let name = "にんじゃわんこ";
 
 // 変数nameの値を出力してください
 console.log(name);
+
+// 変数lengthを定義してください
+let length = 5;
+
+// 変数lengthの値を出力してください
+console.log(length);
+
+// 変数lengthを用いて、円の面積を出力してください
+console.log(length * length * 3);
