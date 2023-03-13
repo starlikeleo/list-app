d51e7c4e815aea7704dad5f16e69df41848730ec
ryo nakagawa <starlikeleon@gmail.com>
2023-03-13 22:40:07 +0900

    add_app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.render('hello.ejs');
 });
 
-// サーバーを起動するコードを貼り付けてください
+app.get('/top', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
+});
+
 app.listen(3000);
